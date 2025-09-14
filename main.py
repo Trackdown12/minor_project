1b830ff03a747aa1be9c65c3b6376c69957b1791
Vinay Kumar <12707vinaykumar.co@gmail.com>
2025-09-14 23:16:36 +0530

    main file

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import sys
 from PyQt5.QtWidgets import QApplication, QMainWindow
-from PyQt5.QtGui import QScreen
+from PyQt5.QtGui import QScreen,QFont,QIcon
 from PyQt5.QtWidgets import QLabel, QPushButton, QTextEdit
 from VOICE import *
 class MainWindow(QMainWindow):
@@ -22,26 +22,32 @@ class MainWindow(QMainWindow):
             screen_width // 2, 
             screen_height // 2
         )
-        self.setWindowTitle("Voice Assistant")
+        self.setWindowTitle("InstagramSpamDetector")
+        # Set Instagram icon (make sure you have a .ico or .png file)
+        self.setWindowIcon(QIcon("Instagram icon.png"))  # path to your icon
+
 
         # Add label
-        self.label = QLabel("Type text below and click Speak:", self)
+        self.label = QLabel("Greetings User check the spam ", self)
         self.label.move(20, 20)
         self.label.resize(300, 30)
+        
+        # Using QFont
+        font = QFont("Arial", 14)      # font family Arial, size 14
+        font.setBold(True)              # make it bold
+        self.label.setFont(font)
+
+        # Optional: color using setStyleSheet
+        self.label.setStyleSheet("color: blue;")
 
         # Add text box
         self.textbox = QTextEdit(self)
-        self.textbox.setGeometry(20, 60, self.width() - 40, 200)
+        self.textbox.setGeometry(60, 60, 500,50)
 
         # Add Greet button
         self.greet_button = QPushButton("Greet Me", self)
         self.greet_button.setGeometry(20, 270, 100, 40)
         self.greet_button.clicked.connect(self.speaker.GreetUser)
-
-    def speak_text(self):
-        text = self.textbox.toPlainText().strip()
-        if text:
-            self.speaker.talk(text)  # synchronous (no threading)
 
 # ---------- Run Application ----------
 if __name__ == "__main__":
